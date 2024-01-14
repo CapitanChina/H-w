ff83496321bbed28ec17fb60c96fdb28a9fb7277
Anna <mr.lindermann@bk.ru>
2024-01-15 01:02:50 +0300

    Палиндром работает с фразой, но без запятых

--- Palindrome.js
+++ Palindrome.js
@@ -7,15 +7,22 @@ return name.toLowerCase()===reverseName
 }
 console.log("palCheck: "+palCheck(name1)); */
 
-const name1 = "Anna";
+const name1 = "А луна канула";
 const name2 = "Marina";
 function palCheck(name) {
 
-let reverseName =  name.toLowerCase().split('');
+let reverseName =  name.toLowerCase().split(' ').join('').split('');
 for (let i = 0; i < reverseName.length; i++) {
     reverseName[i]=reverseName[reverseName.length-(i+1)];
     reverseName[reverseName.length-(i+1)]=reverseName[i];    
 }
-return name.toLowerCase()===reverseName.join ('');
+return name.toLowerCase().split(' ').join('')===reverseName.join('');
+/* return console.log(reverseName.join('')); */
 }
-console.log("palCheck: "+palCheck(name1))
+console.log("palCheck: "+palCheck(name1)); 
+
+
+
+
+/* const name1 = "Шла cfif ijcc";
+console.log(name1.split(' ').join ('').split('')) */
