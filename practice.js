f82ed291497eab94b6a0181b5d710fc635968d7e
Anna <mr.lindermann@bk.ru>
2024-01-17 20:29:16 +0300

    И это победа!

--- practice.js
+++ practice.js
@@ -1,15 +1,19 @@
-let arr = [0, 1, 4, 0, 2, 3, 0]; // arr1 = [0, 1, 2, 1, 0] [0 1 2 2 1 0]
-//console.log(arr);
+let arr = [1, 0, 1, 4, 0, 2, 3, 0]; // arr1 = [0, 1, 2, 1, 0] [0 1 2 2 1 0]
 function stepCheck(house) {
     let zero=0;
     const resultArray = [];
     const zeroArray = [];
     let distance = 0;
+    for (let k = 0; k < house.length; k++) {
+        if (house[k] === 0) {
+            zero = k;
+            break;
+        }         
+    }
     for (let i = 0; i < house.length; i++) {
         if (house[i] === 0) {
             zero = i;
-            distance = i - zero;
-            console.log(zero, i, distance)
+            distance = Math.abs(i - zero);            
             resultArray.push(distance);
         } else {
             for (let j = i + 1; j < house.length; j++) {
@@ -20,16 +24,16 @@ function stepCheck(house) {
                         break;    
                     }                                    
                 } else if (i-zero<=j-i) {
-                    distance = i - zero;
+                    distance = Math.abs(i - zero);
                     resultArray.push(distance);
                     break;
                     }                    
             }
-        }  
-//       console.log(i, zero, i - zero);
-            
+        }              
     }
     return resultArray;
 }
-console.log("resultArray: "+stepCheck(arr));
+console.log("originArray: " + arr);
+console.log("resultArray: " + stepCheck(arr));
+
 //дожать алгоритм
